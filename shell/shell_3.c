693d26111de9f36c732fa99b24344a43504aa203
g0d0g <medrihanlucian@gmail.com>
2019-09-19 13:07:41 +0200

    aggiunte funzioni shell adc

--- shell/shell_3.c
+++ shell/shell_3.c
@@ -9,8 +9,10 @@
 
 
 int shell_set_name(char **args);
-int shell_set_channel_value(char **args);
-int shell_set_channel_name(char **args);
+int shell_set_pwm_value(char **args);
+int shell_set_pwm_name(char **args);
+int shell_get_adc_value(char **args);
+int shell_set_adc_name(char **args);
 int shell_query_channels(char **args);
 int shell_help(char **args);
 int shell_exit(char **args);
@@ -25,16 +27,20 @@ struct termios tio;
 	int tty_fd;
 
 	unsigned char c='D';
-char *help_com[]={  "set_name\t\t to set a name for your device \neg: set_name pippofranco",
-  "set_channel_name\t to set a name for a switch from 0 to 7 \neg: set_channel_name pippofranco switch_3 platinet",
-  "set_channel_value\t to set a value for a switch from 0 to 255\neg: set_channel_value platinet 069\nYou can use ALL as instead of NAME_SWITCH to set all Swtiches to at the same value\neg: set_channel_value ALL 255",
-  "query_channels\t to see all the switches names and values from a device\neg:query_channels platinet",
+char *help_com[]={  "set_name\t\t to set a name for your device \neg: set_name cucina",
+  "set_pwm_name\t to set a name for a PWM from 0 to 7 \neg: set_pwm_name cucina switch_3 microonde",
+  "set_pwm_value\t to set a value for a switch from 0 to 255\neg: set_pwm_value microonde 125\nYou can use ALL instead of NAME_SWITCH \neg: set_pwm_value ALL 255",
+  "set_adc_name\t to set a name for an ADC from 0 to 7\n eg: set_adc_name cucina temperatura",
+  "get_adc_value\t to get a value from the adc \neg: get_adc_value cucina temperatura",
+  "query_channels\t to see all the switches names and values from a device\neg:query_channels microonde",
   "help\t\t\t to see this easy tutorial",
   "exit\t\t\t to exit from the shell"};
 char *builtin_str[] = {
   "set_name",
-  "set_channel_name",
-  "set_channel_value",
+  "set_pwm_name",
+  "set_pwm_value",
+  "set_adc_name",
+  "get_adc_value",
   "query_channels",
   "help",
   "exit"
@@ -42,8 +48,10 @@ char *builtin_str[] = {
 
 int (*builtin_func[]) (char **) = {
   &shell_set_name,
-  &shell_set_channel_name,
-  &shell_set_channel_value,
+  &shell_set_pwm_name,
+  &shell_set_pwm_value,
+  &shell_set_adc_name,
+  &shell_get_adc_value,
   &shell_query_channels,
   &shell_help,
   &shell_exit
@@ -75,15 +83,15 @@ int shell_query_channels(char **args){
 		//invio dati
 		ret = write(tty_fd, buffer, i+3);
 		if(ret < 0) printf("Errore nella write buffer\n");
-		
+
 		readFromArduino();
   }
   return 1;
 }
-int shell_set_channel_value(char **args){
+int shell_set_pwm_value(char **args){
 	int ret;
   if (args[1] == NULL || args[2]==NULL || args[3]!=NULL) {
-    fprintf(stderr, "shell: expected argument to \"set_channel_value\" SWITCH_NAME VALUE_FRM_000_TO_255 \n");
+    fprintf(stderr, "shell: expected argument to \"set_pwm_value\" SWITCH_NAME VALUE_FRM_000_TO_255 \n");
   }
   else if ( args[2][0] == NULL || args[2][1]==NULL || args[2][2]==NULL || args[2][3]!=NULL ){
     fprintf(stderr, "The value is not valid, you have to insert a number between 000 and 255 \n");
@@ -117,18 +125,18 @@ int shell_set_channel_value(char **args){
   return 1;
 }
 
-int shell_set_channel_name(char **args){
+int shell_set_pwm_name(char **args){
 
 	int ret, i=0;
 	char code[2], num_switch;
 	code[0] = '0';
 	code[1] = '1';
   if (args[1] == NULL || args[2] == NULL || args[3] == NULL) {
-    fprintf(stderr, "shell: expected arguments to \"set_channel_name\" CONTROLLER_NAME switch_NUMBER \"SWITCH_NAME\" \n");
+    fprintf(stderr, "shell: expected arguments to \"set_pwm_name\" CONTROLLER_NAME switch_NUMBER \"SWITCH_NAME\" \n");
   } else {
 
 		if(args[2][0]!= 's' || args[2][1]!= 'w'|| args[2][2]!= 'i' || args[2][3]!= 't' || args[2][4]!= 'c' || args[2][5]!= 'h' || args[2][6]!= '_'){
-			fprintf(stderr, "shell: expected arguments to \"set_channel_name\" CONTROLLER_NAME switch_NUMBER \"SWITCH_NAME\" (max length 16) \n");
+			fprintf(stderr, "shell: expected arguments to \"set_pwm_name\" CONTROLLER_NAME switch_NUMBER \"SWITCH_NAME\" (max length 16) \n");
 		}
 		else{
 
@@ -162,9 +170,92 @@ int shell_set_channel_name(char **args){
 
 
   }
-  
+
   return 1;
 }
+////ADC FUNCTIONS
+int shell_get_adc_value(char **args){
+	int ret;
+  if (args[1] == NULL) {
+    fprintf(stderr, "shell: expected argument to \"get_adc_value\"  \n");
+  }
+
+  else{
+
+
+	  for(int i = 0; i < 32; i++){
+		buffer[i] = NULL;
+	  }
+
+    //codice funzione
+	  buffer[0] = '1';
+	  buffer[1] = '2';
+
+	  int i = 0;
+		while(args[1][i] != NULL){
+		buffer[2+i] = args[1][i];
+		i++;
+		}
+		buffer[2+i] = args[2][0];
+
+		ret = write(tty_fd, buffer, i+6);
+		if(ret < 0) printf("Errore nella write buffer\n");
+
+
+    readFromArduino();
+    }
+  return 1;
+}
+
+int shell_set_adc_name(char **args){
+
+	int ret, i=0;
+	char code[2], num_switch;
+	code[0] = '1';       //non so ancora quali codici usiamo
+	code[1] = '1';       //stesso problema
+  if (args[1] == NULL || args[2] == NULL || args[3] == NULL) {
+    fprintf(stderr, "shell: expected arguments to \"set_adc_name\" CONTROLLER_NAME switch_NUMBER \"SWITCH_NAME\" \n");
+  } else {
+
+		if(args[2][0]!= 's' || args[2][1]!= 'w'|| args[2][2]!= 'i' || args[2][3]!= 't' || args[2][4]!= 'c' || args[2][5]!= 'h' || args[2][6]!= '_'){
+			fprintf(stderr, "shell: expected arguments to \"set_adc_name\" CONTROLLER_NAME switch_NUMBER \"SWITCH_NAME\" (max length 16) \n");
+		}
+		else{
+
+    num_switch = args[2][7];
+
+    for(int i = 0; i < 32; i++){
+		buffer[i] = NULL;
+	  }
+
+    //codice funzione
+	  buffer[0] = '1';
+	  buffer[1] = '1';
+    buffer[2] = num_switch;
+    //arg[1]= arduinoname arg[2]=switch arg[3]=channel name
+	  int i = 0;
+		while(args[1][i] != NULL){
+		buffer[3+i] = args[1][i];
+		i++;
+		}
+    buffer[3+i]='*';
+    int j=0;
+    while(args[3][j] != NULL){
+		buffer[4+i+j] = args[3][j];
+		j++;
+		}
+    ret = write(tty_fd, buffer, j+i+5);
+		if(ret < 0) printf("Errore nella write code\n");
+
+		readFromArduino();
+    }
+
+
+  }
+
+  return 1;
+}
+//END OF ADC FUNCTIONS
 
 int shell_set_name(char **args){
 	int ret;
@@ -174,7 +265,7 @@ int shell_set_name(char **args){
 	if (args[1] == NULL) {
 		fprintf(stderr, "shell: expected argument to \"set_name\" CONTRLLER_NAME\n");
 	} else {
-    
+
 		ret = write(tty_fd, code, 2);
 		if(ret < 0) printf("Errore nella write code\n");
 		int i = 0;
@@ -192,7 +283,7 @@ int shell_set_name(char **args){
 
 		ret = write(tty_fd, args[1], i+1);
 		if(ret < 0) printf("Errore nella write\n");
-		
+
 		readFromArduino();
 	}
 	return 1;
@@ -351,13 +442,13 @@ void shell_loop(void)
 
   do {
 	//sleep(1);
-	
+
     printf("smart_house >> ");
     line = shell_read_line();
     args = shell_split_line(line);
     status = shell_execute(args);
-    
-    
+
+
 
     free(line);
     free(args);
@@ -375,7 +466,7 @@ void readFromArduino(){
 		printf("%c", c);
 		if(c=='Y'){
 			gand=1;
-	
+
 		}
 		while(c != '\r' ){
 			if(read(tty_fd,&c,1)>0){
@@ -385,8 +476,8 @@ void readFromArduino(){
 		if(gand){
 		printGandalf();}
 		printf("\n");
-	
-	
+
+
 	/*if(read(tty_fd,&c,1)>0){
 		printf("\nARDUINO: ");
 		printf("%c", c);
@@ -435,4 +526,3 @@ int main(int argc, char **argv){
 
   return EXIT_SUCCESS;
 }
-
